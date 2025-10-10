e3a11896b724c841b3956172b1b5360b5915b315
Oscar-McCumiskey <ommccumiskey@gmail.com>
2025-10-10 11:06:44 +0100

    Models Imported

---
+++ Source/DES510_Team_Uist/ShockGame/SmartHomeAppliance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PossessableAppliance.h"
+#include "SmartHomeAppliance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DES510_TEAM_UIST_API ASmartHomeAppliance : public APossessableAppliance
+{
+	GENERATED_BODY()
+	
+};
