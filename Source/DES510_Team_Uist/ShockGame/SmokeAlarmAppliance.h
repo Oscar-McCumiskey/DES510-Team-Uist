39f10962a90baf032d81153fd73f4f0c78e9ac01
Oscar-McCumiskey <ommccumiskey@gmail.com>
2025-10-30 12:15:18 +0000

    Create appliance classes

---
+++ Source/DES510_Team_Uist/ShockGame/SmokeAlarmAppliance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PossessableAppliance.h"
+#include "SmokeAlarmAppliance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DES510_TEAM_UIST_API ASmokeAlarmAppliance : public APossessableAppliance
+{
+	GENERATED_BODY()
+	
+};
